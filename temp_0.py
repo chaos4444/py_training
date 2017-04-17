dc2b9cb0dd9890bc427f2822ce808e788195cb6e
Chaos 4444 <damian4_444@o2.pl>
2017-04-17 17:00:01 +0200

    dicts

--- temp_0.py
+++ temp_0.py
@@ -1,5 +1,8 @@
-num_1 = input("enter something: ")
+# ========
+# begin
 
+#num_1 = input("enter something: ")
+num_1=1 #tmp
 if num_1 > 10:
 	print "gt 10"
 else:
@@ -13,8 +16,12 @@ def formula(a,b):
 	result = (a+b)/b
 	return result
 
+# ========
+# loops
+
 print(formula(float(34),num_1))
-cut = input ("specify cut index: ")
+#cut = input ("specify cut index: ")
+cut=1 #tmp
 index = 0
 
 while index < cut:
@@ -22,11 +29,71 @@ while index < cut:
 	index += 1
 
 
-cut = input ("specify cut index: ")
+#cut = input ("specify cut index: ")
 
 for index in range (0,cut):
 	print "index: " + str(index)
 	index += 1
 
+# ========
+# lists
 
+list = [11,23,443,22,11333]
+
+print list[0]
+print list[-1] #negative starts from left
+print list
+print len(list)
+print list[0:2]
+print list[1:2]
+
+list.append(90)
+print list
+print len(list)
+
+list.pop(2)
+print list
+print len(list)
+
+def sumv(a):
+	result = 0
+	for x in a:
+		result = result+x
+	return result
+
+def avgv(a):
+	return sumv(a)/len(a)
+
+print sumv(list)
+print avgv(list)
+
+# ========
+# dictionaries
+
+dict = {"user1":"pass1","user2":"pass2","user3":"pass3","userpp":"qwerty"}
+print dict
+print dict["user1"]
+print "\n"
+
+for x in dict:
+	print x
+
+def exists_in_dict(d,key):
+	result = False
+	for x in d:
+		if x == key:
+			result = True
+	return result
+
+exitv = False
+while not exitv :
+	inp = raw_input("Enter user name: ")
+#	if exists_in_dict(dict,inp):
+	if inp in dict:
+		print "Tricky one :-). The password for the " + inp + " is: " + dict[inp]
+	else:
+		print " Yyyy... WTF? o.O"
+		if inp == 'q':
+			print "exiting"
+			break
 
